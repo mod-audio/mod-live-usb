cf02420d5710ae2235fcfa030dca7a88a282177d
falkTX <falktx@falktx.com>
2021-04-18 02:03:40 +0100

    Some little tweaks
    Signed-off-by: falkTX <falktx@falktx.com>

--- archiso/build.sh
+++ archiso/build.sh
@@ -4,23 +4,19 @@ set -e
 
 cd $(dirname ${0})
 
-# docker build -t mod-live-usb_iso .
+docker build -t mod-live-usb_iso .
 
 mkdir -p output workdir
-mkdir -p liveusb/airootfs/mnt/live-welcome
 mkdir -p liveusb/airootfs/mnt/mod-os
 mkdir -p liveusb/airootfs/mnt/plugins
 mkdir -p liveusb/airootfs/root
 
-# mount our custom files (so we dont have to copy stuff)
+# mount plugin dir (so we dont have to copy stuff)
 if [ ! -e liveusb/airootfs/mnt/plugins/abGate.lv2 ]; then
     sudo mount --bind ../plugins/bundles liveusb/airootfs/mnt/plugins
 fi
-if [ ! -e liveusb/airootfs/mnt/live-welcome/Makefile ]; then
-    sudo mount --bind ../live-welcome liveusb/airootfs/mnt/live-welcome
-fi
 
-# extract container rootfs
+# extract container rootfs (FIXME non-exec binaries because of this?)
 if [ ! -e liveusb/airootfs/mnt/mod-os/bin ]; then
     tar xf ../mod-os/rootfs.tar -C liveusb/airootfs/mnt/mod-os
 fi
@@ -29,18 +25,20 @@ fi
 cp -r ../mod-os/config ../mod-os/overlay-files liveusb/airootfs/root/
 cp ../mod-os/start.sh liveusb/airootfs/root/start.sh
 
-# generate live-welcome
-if [ ! -e liveusb/airootfs/mnt/live-welcome/mod-live-usb-welcome ]; then
+# generate live-welcome binary
+if [ ! -e liveusb/airootfs/root/mod-live-usb-welcome ]; then
     docker run \
-        -v ${PWD}/liveusb:/opt/mount/liveusb \
+        -v ${PWD}/../live-welcome:/opt/mount/live-welcome \
         --rm mod-live-usb_iso:latest \
-        /usr/bin/make -C /opt/mount/liveusb/airootfs/mnt/live-welcome
+        /usr/bin/make -C /opt/mount/live-welcome
+    cp ${PWD}/../live-welcome/mod-live-usb-welcome liveusb/airootfs/root/mod-live-usb-welcome
 fi
 
 # make sure to regen things
 rm -f output/*.iso
 rm -f workdir/build._*
 
+# let's go!
 docker run --privileged \
   -v ${PWD}/liveusb:/opt/mount/liveusb \
   -v ${PWD}/output:/opt/mount/output \
@@ -48,4 +46,4 @@ docker run --privileged \
   --rm mod-live-usb_iso:latest \
   /usr/bin/mkarchiso -L "Live-USB" -o /opt/mount/output -w /opt/mount/workdir -v /opt/mount/liveusb
 
-# ... -platform linuxfb --no-sandbox
+# ... -platform linuxfb -style fusion --no-sandbox
--- archiso/liveusb/profiledef.sh
+++ archiso/liveusb/profiledef.sh
@@ -14,8 +14,8 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/mnt/live-welcome"]="0:0:755"
   ["/root"]="0:0:750"
+  ["/root/live-welcome"]="0:0:755"
   ["/root/start.sh"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
 )
