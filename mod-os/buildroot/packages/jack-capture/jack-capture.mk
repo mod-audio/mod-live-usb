3a6317a060af407b81ef45a190edf9a1ec065c0c
falkTX <falktx@falktx.com>
2021-04-21 00:10:54 +0100

    Update jack-capture to latest git
    Signed-off-by: falkTX <falktx@falktx.com>

--- mod-os/buildroot/packages/jack-capture/jack-capture.mk
+++ mod-os/buildroot/packages/jack-capture/jack-capture.mk
@@ -4,14 +4,13 @@
 #
 ################################################################################
 
-JACK_CAPTURE_VERSION = 0.9.71
-JACK_CAPTURE_SITE = http://archive.notam02.no/arkiv/src
-JACK_CAPTURE_SOURCE = jack_capture-$(JACK_CAPTURE_VERSION).tar.gz
-JACK_CAPTURE_DEPENDENCIES = jack2mod libsndfile
+# JACK_CAPTURE_VERSION = 0.9.71
+# JACK_CAPTURE_SITE = http://archive.notam02.no/arkiv/src
+# JACK_CAPTURE_SOURCE = jack_capture-$(JACK_CAPTURE_VERSION).tar.gz
+JACK_CAPTURE_VERSION = e2c0c3d25fce0a13f3f18c622079cd29178af7f8
+JACK_CAPTURE_SITE = $(call github,kmatheussen,jack_capture,$(JACK_CAPTURE_VERSION))
 
-ifeq ($(BR2_cortex_a7),y)
-JACK_CAPTURE_EXTRA_FLAGS = -fprefetch-loop-arrays -funroll-loops -funsafe-loop-optimizations
-endif
+JACK_CAPTURE_DEPENDENCIES = jack2mod libsndfile
 
 JACK_CAPTURE_TARGET_MAKE = \
 	$(TARGET_MAKE_ENV) \
