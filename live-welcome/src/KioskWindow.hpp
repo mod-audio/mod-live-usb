5de5ae31bb34086095ba578c972e165f2b71e237
falkTX <falktx@falktx.com>
2021-06-25 13:16:14 +0100

    Start adding sys host stuff
    Signed-off-by: falkTX <falktx@falktx.com>

--- live-welcome/src/KioskWindow.hpp
+++ live-welcome/src/KioskWindow.hpp
@@ -18,37 +18,67 @@
 
 #include "../widgets/digitalpeakmeter.hpp"
 
+#define SERVER_MODE
+extern "C" {
+#include "sys_host/sys_host_impl.h"
+}
+
 class PeakMeterThread : public QThread
 {
     AudioContainerComm* containerComm;
     DigitalPeakMeter& peakMeterIn;
     DigitalPeakMeter& peakMeterOut;
 
+    int sys_host_shmfd;
+    sys_serial_shm_data* sys_host_data;
+
 public:
     explicit PeakMeterThread(QObject* const parent, DigitalPeakMeter& in, DigitalPeakMeter& out)
       : QThread(parent),
         containerComm(nullptr),
         peakMeterIn(in),
-        peakMeterOut(out) {}
+        peakMeterOut(out),
+        sys_host_shmfd(-1),
+        sys_host_data(nullptr) {}
 
     void init()
     {
         if ((containerComm = initAudioContainerComm()) != nullptr)
+        {
+            if (sys_serial_open(&sys_host_shmfd, &sys_host_data))
+                fprintf(stdout, "sys_host shared memory ok!\n");
+            else
+                fprintf(stderr, "sys_host shared memory failed\n");
+
             start(HighPriority);
+        }
     }
 
     void stop()
     {
         cleanupAudioContainerComm(containerComm);
+        sys_serial_close(sys_host_shmfd, sys_host_data);
         wait(2000);
     }
 
     void run() override
     {
         float peaks[4];
+        char msg[SYS_SERIAL_SHM_DATA_SIZE];
+        sys_serial_event_type etype;
+        uint8_t page, subpage;
+
+        sys_serial_shm_data_channel* const sysdata = sys_host_data != nullptr ? &sys_host_data->server : nullptr;
 
         while (containerComm != nullptr && ! isInterruptionRequested())
         {
+            // flush all incoming host events
+            if (sys_host_data != nullptr && sem_trywait(&sysdata->sem) == 0)
+            {
+                while (sysdata->head != sysdata->tail)
+                    sys_serial_read(sysdata, &etype, &page, &subpage, msg);
+            }
+
             if (! waitForAudioContainerComm(containerComm))
                 continue;
 
@@ -145,7 +175,7 @@ public:
             peakMeterThread.init();
 
         if (getenv("TESTING") != nullptr)
-            tabWidget.reloadPage();
+            tryConnectingToWebServer();
     }
 
     ~KioskWindow() override
--- mod-os/start-dummy.sh
+++ mod-os/start-dummy.sh
@@ -9,64 +9,6 @@ if [ ! -e /proc/asound/Dummy ]; then
     echo "error: can't find Dummy soundcard"
     exit 1
 fi
-# sudo modprobe snd-dummy hrtimer=1 pcm_devs=1 pcm_substreams=8
+# sudo modprobe snd-dummy hrtimer=1 pcm_devs=1 pcm_substreams=1
 
-# get soundcard index
-SOUNDCARD=$(readlink /proc/asound/Dummy | awk 'sub("card","")')
-
-# pass soundcard setup into container
-echo "# mod-live-usb soundcard setup
-SOUNDCARD=${SOUNDCARD}
-SAMPLERATE=48000
-BUFFERSIZE=256
-NPERIODS=2
-CAPTUREARGS=
-PLAYBACKARGS=-P
-EXTRAARGS=-o 2
-" > $(pwd)/config/soundcard.sh
-
-# no security, yay?
-export SYSTEMD_SECCOMP=0
-
-# optional nspawn options (everything must be valid)
-NSPAWN_OPTS=""
-if [ -e /dev/snd/pcmC${SOUNDCARD}D0c ]; then
-NSPAWN_OPTS+=" --bind=/dev/snd/pcmC${SOUNDCARD}D0c"
-fi
-if [ -e /dev/snd/pcmC${SOUNDCARD}D0p ]; then
-NSPAWN_OPTS+=" --bind=/dev/snd/pcmC${SOUNDCARD}D0p"
-fi
-if [ -e /mnt/pedalboards ]; then
-NSPAWN_OPTS+=" --bind-ro=/mnt/pedalboards"
-fi
-if [ -e /mnt/plugins ]; then
-NSPAWN_OPTS+=" --bind-ro=/mnt/plugins"
-elif [ -e ../plugins/bundles/abGate.lv2 ]; then
-NSPAWN_OPTS+=" --bind-ro=$(pwd)/../plugins/bundles:/mnt/plugins"
-fi
-
-# ready!
-sudo systemd-nspawn \
---boot \
---read-only \
---capability=all \
---private-users=false \
---resolv-conf=bind-host \
---machine="mod-live-usb" \
---image=$(pwd)/rootfs.ext2 \
---bind=/dev/snd/controlC${SOUNDCARD} \
---bind=/dev/snd/seq \
---bind=/dev/snd/timer \
---bind=$(pwd)/../rwdata/root:/root \
---bind=$(pwd)/../rwdata/user-files:/data/user-files \
---bind-ro=/etc/hostname \
---bind-ro=/etc/hosts \
---bind-ro=$(pwd)/config:/mnt/config \
---bind-ro=$(pwd)/overlay-files/etc/group:/etc/group \
---bind-ro=$(pwd)/overlay-files/etc/passwd:/etc/passwd \
---bind-ro=$(pwd)/overlay-files/etc/shadow:/etc/shadow \
---bind-ro=$(pwd)/overlay-files/system:/etc/systemd/system \
---tmpfs=/tmp \
---tmpfs=/var ${NSPAWN_OPTS}
-
-# --tmpfs=/run \
+exec bash ./start.sh Dummy
