c1013100cd1c756748d38518cc1b69db2ad14947
falkTX <falktx@falktx.com>
2022-04-01 22:41:18 +0100

    Fix missing peakmeter, misc changes Fixes #6
    Signed-off-by: falkTX <falktx@falktx.com>

--- mod-os/buildroot/packages/mod-ui/mod-ui.mk
+++ mod-os/buildroot/packages/mod-ui/mod-ui.mk
@@ -6,7 +6,7 @@
 
 MOD_UI_VERSION = f13e948be12d4c395980893600b8f54372e95e59
 MOD_UI_SITE = $(call github,moddevices,mod-ui,$(MOD_UI_VERSION))
-MOD_UI_DEPENDENCIES = python3 python-aggdraw python-pillow python-pycrypto python-pystache python-setuptools python-serial python-tornado host-python3 jack2mod lilv
+MOD_UI_DEPENDENCIES = python3 python-aggdraw python-pillow python-pycrypto python-pystache python-setuptools python-serial python-tornado host-python3 jack-capture jack2mod lilv mod-host
 MOD_UI_SETUP_TYPE = distutils
 MOD_UI_ENV = CXX=$(TARGET_CXX)
 
