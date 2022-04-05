c9ef4b8202dd6159100a5512e631fac249c487a2
falkTX <falktx@falktx.com>
2022-04-05 11:05:23 +0100

    Update mod-ui
    Signed-off-by: falkTX <falktx@falktx.com>

--- mod-os/buildroot/packages/mod-ui/mod-ui.mk
+++ mod-os/buildroot/packages/mod-ui/mod-ui.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-MOD_UI_VERSION = f13e948be12d4c395980893600b8f54372e95e59
+MOD_UI_VERSION = 6c0d2717ba041d7d65e967acfbcc148437b30bf5
 MOD_UI_SITE = $(call github,moddevices,mod-ui,$(MOD_UI_VERSION))
 MOD_UI_DEPENDENCIES = python3 python-aggdraw python-pillow python-pycrypto python-pystache python-setuptools python-serial python-tornado host-python3 jack-capture jack2mod lilv mod-host
 MOD_UI_SETUP_TYPE = distutils
