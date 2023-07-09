20808105b5e71acbb31644efd93dd8131fa45c82
falkTX <falktx@falktx.com>
2023-07-09 12:36:05 +0200

    Custom empty pedalboard
    Signed-off-by: falkTX <falktx@falktx.com>

--- mod-os/config/environment.sh
+++ mod-os/config/environment.sh
@@ -1,5 +1,6 @@
 LD_BIND_NOW=1
 LV2_PATH=/root/.lv2:/usr/lib/lv2:/mnt/plugins
+MOD_DEFAULT_PEDALBOARD=/mnt/pedalboards/default.pedalboard
 MOD_DEV_HMI=1
 MOD_FACTORY_PEDALBOARDS_DIR=/mnt/pedalboards
 MOD_KEYS_PATH=/root/keys/
@@ -10,4 +11,3 @@ SNDFILE_JACKPLAY_AUTOCONNECT=mod-host:monitor-in_%d
 MOD_DEVICE_WEBSERVER_PORT=8888
 MOD_FILE_MANAGER_PORT=8081
 MOD_HOST_SOCKET_PORT=5555
-
